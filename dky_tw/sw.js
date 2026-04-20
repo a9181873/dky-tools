c77cc252d9434c8b6526c3cff5b31259ff5b1e93
a9181873 <a9181873@gmail.com>
2026-04-20 23:52:56 +0800

    Bump service worker cache version to invalidate old Hash routing files

--- dky_tw/sw.js
+++ dky_tw/sw.js
@@ -1,5 +1,5 @@
 // Simple Service Worker: precache core assets and enable offline-first navigation
-const CACHE_NAME = 'dky-tools-v1';
+const CACHE_NAME = 'dky-tools-v2';
 const PRECACHE_URLS = [
   '/',
   '/index.html',
