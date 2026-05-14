f0562d024b18ed53ca7a3c926b56fe4ad265c769
a9181873 <a9181873@gmail.com>
2026-05-14 23:49:20 +0800

    強制更新 SW cache 到 v7

--- dky_use/sw.js
+++ dky_use/sw.js
@@ -1,5 +1,5 @@
 // Simple Service Worker: precache core assets and enable offline-first navigation
-const CACHE_NAME = 'dky-tools-v6';
+const CACHE_NAME = 'dky-tools-v7';
 const PRECACHE_URLS = [
   '/',
   '/index.html',
