342c2bbfd9fc6abfdcd665b9acba9f5f72d12cc5
a9181873 <a9181873@gmail.com>
2026-05-16 01:04:33 +0800

    Bump SW to v9

--- dky_use/sw.js
+++ dky_use/sw.js
@@ -1,5 +1,5 @@
 // Simple Service Worker: precache core assets and enable offline-first navigation
-const CACHE_NAME = 'dky-tools-v8';
+const CACHE_NAME = 'dky-tools-v9';
 const PRECACHE_URLS = [
   '/',
   '/index.html',
